38ddf968ee5bfc9d5b4e762ee94e53b8ec29bb45
Денис Денисов <xeddyx@mail.ru>
2022-11-09 20:52:31 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int ticketPrice = 600;
+        int mileBonus = 20;
+        int miles = ticketPrice / mileBonus;
+        System.out.println("количество бонусных миль:" + miles);
+
+    }
+}
